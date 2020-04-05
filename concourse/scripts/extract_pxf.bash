49da322c479e5ce9f153395443a51defbb808555
Oliver Albertini <oalbertini@vmware.com>
2020-04-04 20:01:45 -0700

    Add script that initializes GPDB
    Authored-by: Oliver Albertini <oalbertini@vmware.com>

--- concourse/scripts/extract_pxf.bash
+++ concourse/scripts/extract_pxf.bash
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+# this script removes the C client extension from GPDB binary
 # run as gpadmin
 # can run in centos6 or centos7 environments
+
 set -e
 
 : "${GPDB_PKG:?GPDB_PKG must be set}"
@@ -14,10 +16,10 @@ mkdir -p "${EXTRACT_DIR}"
 pushd "${EXTRACT_DIR}"
 if [[ ${GPDB_PKG} =~ .*\.rpm$ ]]; then
 	# https://stackoverflow.com/a/18787544
-	rpm2cpio "${BASE_DIR}/${GPDB_PKG}" | cpio -idmv
+	rpm2cpio "${BASE_DIR}/${GPDB_PKG}" | cpio -idm
 elif [[ ${GPDB_PKG} =~ .*\.deb$ ]]; then
 	ar x "${BASE_DIR}/${GPDB_PKG}"
-	tar xvf data.tar.xz
+	tar xf data.tar.xz
 else
 	echo "${BASE_DIR}/${GPDB_PKG} is not an RPM or DEB file"
 	exit 1
@@ -34,10 +36,13 @@ list_of_pxf_files=(
 )
 
 for file in "${list_of_pxf_files[@]}"; do
-	[[ -e "${file}" ]] || { echo "${file} not found in GPDB archive, skipping..."; continue; }
+	if ! [[ -e "${file}" ]]; then
+		echo "${file} not found in GPDB archive, skipping..."
+		continue
+	fi
 	echo "removing ${file} from GPDB archive"
 	rm -rf "${file}"
 done
 
-tar zcvf "${BIN_GPDB_DIR}/bin_gpdb.tar.gz" -C "${EXTRACT_DIR}" "${gpdb_home#"${EXTRACT_DIR}"}"
+tar zcf "${BIN_GPDB_DIR}/bin_gpdb.tar.gz" -C "${EXTRACT_DIR}" "${gpdb_home#"${EXTRACT_DIR}"}"
 echo "/${gpdb_home#"${EXTRACT_DIR}"}" > "${BIN_GPDB_DIR}/GPHOME"
--- concourse/scripts/install_pxf_protocol_extension.bash
+++ concourse/scripts/install_pxf_protocol_extension.bash
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 
-# run as root
-# should run on correct OS for Greenplum installation
-# ubuntu18, centos{6,7}
+# this script installs the pxf protocol extension and rebundles GPDB into a
+# tarball
+# run it as root
+# should run on correct OS for Greenplum installation, e.g. ubuntu18,
+# centos{6,7}
 set -e
 
 : "${BIN_GPDB_DIR:?BIN_GPDB_DIR must be set}"
 : "${PXF_PROTOCOL_EXTENSION_SRC:?PXF_PROTOCOL_EXTENSION_SRC must be set}"
 
-tar zxvf "${BIN_GPDB_DIR}/bin_gpdb.tar.gz" -C /
+tar zxf "${BIN_GPDB_DIR}/bin_gpdb.tar.gz" -C /
 
 source "$(< "${BIN_GPDB_DIR}/GPHOME")/greenplum_path.sh"
 
 USE_PGXS=1 make -C "${PXF_PROTOCOL_EXTENSION_SRC}" install
 
-tar zcvf "${BIN_GPDB_DIR}/bin_gpdb.tar.gz" -C / "${GPHOME}"
+tar zcf "${BIN_GPDB_DIR}/bin_gpdb.tar.gz" -C / "${GPHOME}"
